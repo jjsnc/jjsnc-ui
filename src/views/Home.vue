041dabd97f1184b9cf48c59ddab38535dbb4d390
jjsnc <zhuowangyi@163.com>
2019-05-16 20:20:46 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,14 +22,28 @@
       :hollow-style="true"
     >Styled Checkbox</jjsnc-checkbox>
     <jjsnc-checkbox v-model="checked" :option="option"/>
-    <jjsnc-checkbox v-model="checked" :option="{disabled: true}">Disabled Checkbox</jjsnc-checkbox> -->
+    <jjsnc-checkbox v-model="checked" :option="{disabled: true}">Disabled Checkbox</jjsnc-checkbox>-->
 
     <!-- <jjsnc-checkbox class="with-click" v-model="checked">
       Agree
       <a href="javascript:;" @click.stop="myhandle">处理事件</a>
-    </jjsnc-checkbox> -->
+    </jjsnc-checkbox>-->
     <!-- <jjsnc-checkbox-group v-model="checkList" @input='EVENT_INPUT' :options="options" ></jjsnc-checkbox-group> -->
-    <jjsnc-checkbox-group v-model="checkList" :options="options" :horizontal="true" shape="square" :hollow-style="true" />
+    <!-- <jjsnc-checkbox-group v-model="checkList" :options="options" :horizontal="true" shape="square" :hollow-style="true" /> -->
+    <jjsnc-checkbox-group v-model="checkList">
+      <jjsnc-checkbox option="1">
+        <i>Checkbox 1</i>
+      </jjsnc-checkbox>
+      <jjsnc-checkbox option="2">
+        <i>Checkbox 2</i>
+      </jjsnc-checkbox>
+      <jjsnc-checkbox :option="{value: '3', disabled: true}">
+        <i>Disabled Checkbox</i>
+      </jjsnc-checkbox>
+      <jjsnc-checkbox :option="{value: '4', disabled: true}">
+        <i>Disabled & Checked Checkbox</i>
+      </jjsnc-checkbox>
+    </jjsnc-checkbox-group>
   </div>
 </template>
 
@@ -38,7 +52,7 @@
 // import jjsncButton from "@/components/button/button.vue";
 // import jjsncLoading from "@/components/loading/loading.vue";
 // import jjsncTip from "@/components/tip/tip.vue";
-// import jjsncCheckbox from "@/components/checkbox/checkbox.vue";
+import jjsncCheckbox from "@/components/checkbox/checkbox.vue";
 import jjsncCheckboxGroup from "@/components/checkbox-group/checkbox-group.vue";
 export default {
   name: "home",
@@ -46,23 +60,23 @@ export default {
     // jjsncButton,
     // jjsncLoading,
     // jjsncTip,
-    // jjsncCheckbox,
+    jjsncCheckbox,
     jjsncCheckboxGroup
   },
   data() {
     return {
-      checkList: ['1', '4'],
+      checkList: ["1", "4"],
       options: [
-        '1',
-        '2',
+        "1",
+        "2",
         {
-          label: '3',
-          value: '3',
+          label: "3",
+          value: "3",
           disabled: true
         },
         {
-          label: '4',
-          value: '4',
+          label: "4",
+          value: "4",
           disabled: true
         }
       ]
@@ -70,17 +84,13 @@ export default {
   },
   methods: {
     myhandle() {
-
-      alert('234')
     },
-    EVENT_INPUT(data){
-      console.log(data)
+    EVENT_INPUT() {
     }
   }
 };
 </script>
 <style>
-
 </style>
 
 
