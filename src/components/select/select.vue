4d7dcfa5befcee6dbe4a43c553f615bd9840fba1
jjsnc <zhuowangyi@163.com>
2019-05-26 10:03:57 +0800

    init

---
+++ src/components/select/select.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="jjsnc-select"
+    :class="{'jjsnc-select_active':active,'jjsnc-select_disabled':disabled}"
+    @click="showPicker"
+  >
+    <span v-if="selectedText" class="jjsnc-select-placeholder">{{_selectedText}}</span>
+    <span v-else class="jjsnc-select-placeholder">{{_placeholder}}</span>
+    <i class="jjsnc-select-icon"></i>
+  </div>
+</template>
+<script>
+import {fondIndex} from '../../common/helpers/util';
+import localeMixin from '../../common/mixins/locale'
+export default {};
+</script>
+<style lang="scss">
+</style>
