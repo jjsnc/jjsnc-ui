74f1b54844342330997f4b91733f08ed1b718645
jjsnc <zhuowangyi@163.com>
2019-05-26 10:40:28 +0800

    init

--- src/common/lang/date.js
+++ src/common/lang/date.js
@@ -1,6 +1,6 @@
-const DAY_TIMESTAMP = 60 * 60 * 24 * 1000;
-const HOUR_TIMESTAMP = 60 * 60 * 1000;
-const MUNUTE_TIMESTAMP = 60 * 1000;
+const DAY_TIMESTAMP = 60 * 60 * 24 * 1000
+const HOUR_TIMESTAMP = 60 * 60 * 1000
+const MINUTE_TIMESTAMP = 60 * 1000
 
 function formatType(type, format, value, regExpAttributes) {
     const regExpMap = {
@@ -11,19 +11,19 @@ function formatType(type, format, value, regExpAttributes) {
         minute: '(m+)',
         second: '(s+)',
         quarter: '(q+)',
-        millisecond: '(S+)',
+        millisecond: '(S)'
     }
     if (new RegExp(regExpMap[type], regExpAttributes).test(format)) {
         const replaceStr = type === 'year'
             ? value.toString().substr(4 - RegExp.$1.length)
             : (RegExp.$1.length === 1) ? value : pad(value)
-        format = format.replace(RegExp.$1, replaceStr);
+        format = format.replace(RegExp.$1, replaceStr)
     }
     return format
 }
 
 function pad(value) {
-    return ('00' + value).substr(('' + value).length);
+    return ('00' + value).substr(('' + value).length)
 }
 
 function formatDate(date, format) {
@@ -33,7 +33,7 @@ function formatDate(date, format) {
             regExpAttributes: 'i'
         },
         month: {
-            value: date.getMonth + 1
+            value: date.getMonth() + 1
         },
         date: {
             value: date.getDate(),
@@ -57,9 +57,18 @@ function formatDate(date, format) {
             value: date.getMilliseconds()
         }
     }
+
     for (const key in map) {
-        format = formatType(key, format, map[key].value, map.regExpAttributes)
+        format = formatType(key, format, map[key].value, map[key].regExpAttributes)
     }
-    return format;
+    return format
 }
+
+function getZeroStamp(date) {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return +new Date(year + '/' + month + '/' + day + '00:00:00')
+}
+
 
