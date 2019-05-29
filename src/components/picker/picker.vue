412e0a1f88edf3eba472c8453c050dac9bd7e91e
jjsnc <zhuowangyi@163.com>
2019-05-29 14:50:57 +0800

    init

--- src/components/picker/picker.vue
+++ src/components/picker/picker.vue
@@ -53,9 +53,10 @@ import Bscroll from "better-scroll";
 import jjsncPopup from "../popup/popup.vue";
 import visibilityMixin from "../../common/mixins/visibility";
 import popupMixin from "../../common/mixins/popup";
-import basicPickerMixin from '../../common/mixins/basic-picker';
-  import pickerMixin from '../../common/mixins/picker'
+import basicPickerMixin from "../../common/mixins/basic-picker";
+import pickerMixin from "../../common/mixins/picker";
 import localeMixin from "../../common/mixins/locale";
+import popup from "../../common/mixins/popup";
 const COMPONENT_NAME = "jjsnc-picker";
 
 const EVENT_SELECT = "select";
@@ -64,7 +65,224 @@ const EVENT_CANCEL = "cancel";
 const EVENT_CHANGE = "change";
 
 export default {
-  data() {},
+  name: COMPONENT_NAME,
+  mixins: [visibilityMixin, popup, basicPickerMixin, pickerMixin, localeMixin],
+  props: {
+    pending: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      finalData: this.data.slice()
+    };
+  },
+  created() {
+    this._values = [];
+    this._indexes = this.selectedIndex;
+  },
+  methods: {
+    confirm() {
+      if (!this._canConfirm()) {
+        return;
+      }
+      this.hide();
+      let changed = false;
+      let pickerSelectedText = [];
+
+      const length = this.finalData.length;
+      const oldLength = this._values.length;
+
+      // when column count has changed.
+      if (oldLength !== length) {
+        changed = true;
+        oldLength > length &&
+          (this._values.length = this._indexes.length = length);
+      }
+
+      for (let i = 0; i < length; i++) {
+        let index = this.wheels[i].getSelectedIndex();
+        this._indexes[i] = index;
+
+        let value = null;
+        let text = "";
+        if (this.finalData[i].length) {
+          value = this.finalData[i][index][this.valueKey];
+          text = this.finalData[i][index][this.textKey];
+        }
+        if (this._values[i] !== value) {
+          changed = true;
+        }
+        this._values[i] = value;
+        pickerSelectedText[i] = text;
+      }
+      this.$emit(EVENT_SELECT, this._values, this._indexes, pickerSelectedText);
+      if (changed) {
+        this.$emit(
+          EVENT_VALUE_CHANGE,
+          this._values,
+          this._indexes,
+          pickerSelectedText
+        );
+      }
+    },
+    maskClick() {
+      this.maskClosable && this.cancel();
+    },
+    cancel() {
+      this.hide();
+      this.$emit(EVENT_CANCEL);
+    },
+    show() {
+      if (this.isVisible) {
+        return;
+      }
+      this.isVisible = true;
+      if (!this.wheels || this.dirty) {
+        this.$nextTick(() => {
+          this.wheels = this.wheels || [];
+          let wheelWrapper = this.$refs.wheelWrapper;
+          for (let i = 0; i < this.finalData.length; i++) {
+            this._createWheel(wheelWrapper, i).enable();
+            this.wheels[i].wheelTo(this._indexes[i]);
+          }
+          this.dirty && this._destroyExtraWheels();
+          this.dirty = false;
+        });
+      } else {
+        for (let i = 0; i < this.finalData.length; i++) {
+          this.wheels[i].enable();
+          this.wheels[i].wheelTo(this._indexes[i]);
+        }
+      }
+    },
+    hide() {
+      if (!this.isVisible) {
+        return;
+      }
+      this.isVisible = false;
+      for (let i = 0; i < this.finalData.length; i++) {
+        this.wheels[i].disable();
+      }
+    },
+    setData(data, selectedIndex) {
+      this._indexes = selectedIndex ? [...selectedIndex] : [];
+      this.finalData = data.slice();
+      if (this.isVisible) {
+        this.$nextTick(() => {
+          const wheelWrapper = this.$refs.wheelWrapper;
+          this.finalData.forEach((item, i) => {
+            this._createWheel(wheelWrapper, i);
+            this.wheels[i].wheelTo(this._indexes[i]);
+          });
+          this._destroyExtraWheels();
+        });
+      } else {
+        this.dirty = true;
+      }
+    },
+    refill(datas) {
+      let ret = [];
+      if (!datas.length) {
+        return ret;
+      }
+      datas.forEach((data, index) => {
+        ret[index] = this.refillColumn(index, data);
+      });
+      return ret;
+    },
+    refillColumn(index, data) {
+      const wheelWrapper = this.$refs.wheelWrapper;
+      let scroll = wheelWrapper.children[index].querySelector(
+        ".jjsnc-picker-wheel-scroll"
+      );
+      let wheel = this.wheels ? this.wheels[index] : false;
+      let dist = 0;
+      if (scroll && wheel) {
+        let oldData = this.finalData[index];
+        this.$set(this.finalData, index, data);
+        let selectedIndex = wheel.getSelectedIndex();
+        if (oldData.length) {
+          let oldValue = oldData[selectedIndex][this.valueKey];
+          for (let i = 0; i < data.length; i++) {
+            if (data[i][this.valueKey] === oldValue) {
+              dist = i;
+              break;
+            }
+          }
+        }
+        this._indexes[index] = dist;
+        this.$nextTick(() => {
+          // recreate wheel so that the wrapperHeight will be correct.
+          wheel = this._createWheel(wheelWrapper, index);
+          wheel.wheelTo(dist);
+        });
+      }
+      return dist;
+    },
+    scrollTo(index, dist) {
+      const wheel = this.wheels[index];
+      this._indexes[index] = dist;
+      wheel.wheelTo(dist);
+    },
+    refresh() {
+      this.$nextTick(() => {
+        this.wheels.forEach(wheel => {
+          wheel.refresh();
+        });
+      });
+    },
+    _createWheel(wheelWrapper, i) {
+      if (!this.wheels[i]) {
+        const wheel = (this.wheels[i] = new Bscroll(wheelWrapper.children[i], {
+          wheel: {
+            selectedIndex: this._indexes[i] || 0,
+            wheelWrapperClass: "jjsnc-picker-wheel-scroll",
+            wheelItemClass: "jjsnc-picker-wheel-item"
+          },
+          swipeTime: this.swipeTime,
+          observeDOM: false
+        }));
+        wheel.on("scrollEnd", () => {
+          this.$emit(EVENT_CANCEL, i, wheel.getSelectedIndex());
+        });
+      } else {
+        this.wheels[i].refresh();
+      }
+      return this.wheels[i];
+    },
+    _destroyExtraWheels() {
+      const dataLength = this.finalData.length;
+      if (this.wheels.length > dataLength) {
+        const extraWheels = this.wheels.splice(dataLength);
+        extraWheels.forEach(wheel => {
+          wheel.destory();
+        });
+      }
+    },
+    _canConfirm() {
+      return (
+        !this.pending &&
+        this.wheels.every(wheel => {
+          return !wheel.isInTransition;
+        })
+      );
+    },
+    getFlexOrder(data) {
+      if (data[0]) {
+        return data[0][this.orderKey];
+      }
+      return 0;
+    }
+  },
+  beforeDestroy() {
+    this.wheels &&
+      this.wheels.forEach(wheel => {
+        wheel.destory();
+      });
+    this.wheels = null;
+  },
   components: {
     jjsncPopup
   }
