97167c377c055069a5541a8c768c71be21a7d5fc
jjsnc <zhuowangyi@163.com>
2019-05-30 15:36:32 +0800

    init

---
+++ src/components/rate/rate-item.vue
@@ -0,0 +1,52 @@
+<template>
+  <li class="jjsnc-rate-item" :class="{'jjsnc-rate-item_active':index <=$parent.tempValue}">
+    <slot>
+      <div class="jjsnc-rate-item-def"></div>
+    </slot>
+  </li>
+</template>
+
+<script>
+const COMPONENT_NAME = "jjsnc-rate-item";
+export default {
+  name: COMPONENT_NAME,
+  props: {
+    index: {
+      type: Number,
+      default: 0
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import "../../common/scss/mixin";
+.jjsnc-rate-item {
+  position: relative;
+  width: 32px;
+  flex: 0 1 auto;
+  margin-right: 6px;
+  &::after {
+    contain: "";
+    display: block;
+    padding: 50% 0;
+  }
+  &:last-child {
+    margin-right: 0;
+  }
+  .jjsnc-rate-item-def {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    @include bg-image("evaluation_icon_star_gray");
+  }
+}
+.jjsnc-rate-item_active {
+  .jjsnc-rate-item_def {
+    @include bg-image("evaluation_icon_star");
+  }
+}
+</style>
+
