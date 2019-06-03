c4463fae8305f0c9c6e2fc91be3d795eb2f61bc5
jjsnc <zhuowangyi@163.com>
2019-06-03 10:39:02 +0800

    init

--- src/common/helpers/validator/types.js
+++ src/common/helpers/validator/types.js
@@ -1,6 +1,6 @@
 import { createAddAPI } from '../util'
 
-const DATE_RE = /^(1|2)\d{3}[.\-/]\d{1,2}[.\-/]\d{1,2}$/;
+const DATE_RE = /^(1|2)\d{3}[.\-/]\d{1,2}[.\-/]\d{1,2}$/
 
 const types = {
     string: (val) => {
@@ -9,23 +9,23 @@ const types = {
     number: (val) => {
         return !isNaN(Number(val))
     },
-    arraay: (val) => {
-        return Array.jsArray(val)
+    array: (val) => {
+        return Array.isArray(val)
     },
     date: (val) => {
-        return !isNaN(Number(val) || DATE_RE.test(val));
+        return !isNaN(Number(val)) || DATE_RE.test(val)
     },
     email: (val) => {
         return typeof val === 'string' && /^[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)+$/i.test(val)
     },
     tel: (val) => {
-        return typeof val === 'string' && /^(11|13|14|15|16|17|18|19)[0-9]{9}/.test(val);
+        return typeof val === 'string' && /^(11|13|14|15|17|18|19)[0-9]{9}$/.test(val)
     },
     url: (val) => {
         return typeof val === 'string' && /(https?|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/.test(val)
     }
 }
 
-const addType = createAddAPI(types);
+const addType = createAddAPI(types)
 
 export { types, addType }
