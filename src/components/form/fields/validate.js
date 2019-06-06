53df2bd5131ea852d0eb10b431c25b6f55ada1ac
jjsnc <zhuowangyi@163.com>
2019-06-06 10:27:00 +0800

    init

--- src/components/form/fields/validate.js
+++ src/components/form/fields/validate.js
@@ -1,6 +1,31 @@
-const handleValidateRule = (field, type, custom)=>{
+const handleValidateRule = (field, type, custom) => {
     const ruleValue = field.rules && field.rules[type]
-    if (type !=='custom' ===) {
-        
+    if (type !== 'custom' && ruleValue && typeof ruleValue !== 'function') {
+        field.rules[type] = custom
     }
+}
+
+const createHandler = (type) => {
+    return (field, custom) => {
+        return handleValidateRule(field, type, custom)
+    }
+}
+
+const handleRequired = createHandler(require)
+
+const boolRequeiredHandler = (field) => {
+    return handleRequired(field, (val) => {
+        return val !== false
+    })
+}
+
+const numberGT0RequeiredHandler = (field) => {
+    return handleRequired(field, (val) => {
+        return val > 0
+    })
+}
+
+export {
+    boolRequeiredHandler,
+    numberGT0RequeiredHandler
 }
