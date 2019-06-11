30beb472c3253117d9f915b03404b8a288d0da85
jjsnc <zhuowangyi@163.com>
2019-06-11 15:53:07 +0800

    init

--- src/components/form/components.js
+++ src/components/form/components.js
@@ -1,34 +1,32 @@
-import jjsnceButton from '../button/button.vue'
-import jjsnceCheckbox from '../checkbox/checkbox.vue'
-import jjsnceCheckboxGroup from '../checkbox-group/checkbox-group.vue'
-import jjsnceChecker from '../checker/checker.vue'
-import jjsnceInput from '../input/input.vue'
-import jjsnceRadio from '../radio/radio.vue'
-import jjsnceRadioGroup from '../radio/radio-group.vue'
-import jjsnceRate from '../rate/rate.vue'
-import jjsnceSelect from '../select/select.vue'
-import jjsnceSwitch from '../switch/switch.vue'
-import jjsnceTextarea from '../textarea/textarea.vue'
-import jjsnceUpload from '../upload/upload.vue'
+import jjsncButton from '../button/button.vue'
+import jjsncCheckbox from '../checkbox/checkbox.vue'
+import jjsncCheckboxGroup from '../checkbox-group/checkbox-group.vue'
+import jjsncChecker from '../checker/checker.vue'
+import jjsncInput from '../input/input.vue'
+import jjsncRadio from '../radio/radio.vue'
+import jjsncRadioGroup from '../radio/radio-group.vue'
+import jjsncRate from '../rate/rate.vue'
+import jjsncSelect from '../select/select.vue'
+import jjsncSwitch from '../switch/switch.vue'
+import jjsncTextarea from '../textarea/textarea.vue'
+import jjsncUpload from '../upload/upload.vue'
 
 const allComponents = [
-    jjsnceButton,
-    jjsnceCheckbox,
-    jjsnceCheckboxGroup,
-    jjsnceChecker,
-    jjsnceInput,
-    jjsnceRadio,
-    jjsnceRadioGroup,
-    jjsnceRate,
-    jjsnceSelect,
-    jjsnceSwitch,
-    jjsnceTextarea,
-    jjsnceUpload
+    jjsncButton,
+    jjsncCheckbox,
+    jjsncCheckboxGroup,
+    jjsncChecker,
+    jjsncInput,
+    jjsncRadio,
+    jjsncRadioGroup,
+    jjsncRate,
+    jjsncSelect,
+    jjsncSwitch,
+    jjsncTextarea,
+    jjsncUpload
 ]
 
-
 const components = {}
-
 allComponents.forEach((Component) => {
     components[Component.name] = Component
 })
