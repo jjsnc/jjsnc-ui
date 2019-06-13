d3cfcd253236fc8628b92d77655dcd94de94c9b7
jjsnc <zhuowangyi@163.com>
2019-06-13 16:40:54 +0800

    init

---
+++ src/components/form/form-group.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="jjsnc-form-group">
+    <p class="jjsnc-form-group-legend">{{legend}}</p>
+    <div class="jjsnc-form-group-content">
+      <slot>
+        <jjsnc-form-item v-for="(field, index) in fields" :key="index" :field="field"></jjsnc-form-item>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import jjsncFormItem from "./form-item.vue";
+
+const COMPONENT_NAME = "jsnc-form-group";
+export default {
+  name: COMPONENT_NAME,
+  props: {
+    legend: {
+      type: String,
+      default: ""
+    },
+    fields: {
+      type: Array,
+      default() {
+        /* istanbul ignore next */
+        return [];
+      }
+    }
+  },
+  beforeCreate() {
+    this.form = this.$parent.form;
+  },
+  beforeDestroy() {
+    this.form = null;
+  },
+  components: {
+    jjsncFormItem
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../../common/scss/variable.scss";
+@import "../../common/scss/mixin.scss";
+
+.jjsnc-form-group {
+  overflow: hidden;
+}
+.jjsnc-form-group-legend {
+  font-size: $fontsize-medium;
+  color: $form-group-legend-color;
+  background-color: $form-group-legend-bgc;
+}
+
+</style>
