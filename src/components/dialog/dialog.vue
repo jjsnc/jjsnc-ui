f8f40a74226639619c136a84c4f9b2bb0b39ae12
jjsnc <zhuowangyi@163.com>
2019-06-17 23:39:53 +0800

    init

---
+++ src/components/dialog/dialog.vue
@@ -0,0 +1,79 @@
+<template>
+  <transition name="jjsnc-dialog-fade">
+    <jjsnc-popup
+      type="dialog"
+      :z-index="zIndex"
+      :mask="true"
+      :center="true"
+      v-show="isVisible"
+      @mask-click="maskClick"
+    >
+      <div class="jjsnc-dialog-main">
+        <span class="jjsnc-dialog-close" v-show="showClose" @click="close">
+          <i class="jjsncie-close"></i>
+        </span>
+        <div :class="containerClass">
+          <p class="jjsnc-dialog-icon" v-if="icon">
+            <i :class="icon"></i>
+          </p>
+          <h2 v-if="title|| $slots.title" class="jjsnc-dialog-title">
+            <slot name="title">
+              <p class="jjsnc-dialog-title-def">{{title}}</p>
+            </slot>
+          </h2>
+          <div class="jjsnc-dialog-content">
+            <slot name="center">
+              <div class="jjsnc-dialog-content-def">
+                <p v-html="content" v-if="content">
+                  <jjsnc-input v-bind="prompt" v-model="promptValue" v-if="isPrompt"></jjsnc-input>
+                </p>
+              </div>
+            </slot>
+          </div>
+          <div class="jjsnc-dialog-btns" :class="{'border-right-1px': isConfirm || isPrompt}">
+              <slot name="btns">
+              <a :href="_cancelBtn.href" class="jjsnc-dialog-btn border-top-1px" :class="{'jjsnc-dialog-btn_highlight': _cancelBtn.active, 'jjsnc-dialog-btn_disabled': _cancelBtn.disabled}" v-if="isConfirm || isPrompt" @click="cancel">{{_cancelBtn.text}}</a>
+              <a :href="_confirmBtn.href" class="jjsnc-dialog-btn border-top-1px" :class="{'jjsnc-dialog-btn_highlight': _confirmBtn.active, 'jjsnc-dialog-btn_disabled': _confirmBtn.disabled}" @click="confirm">{{_confirmBtn.text}}</a>
+              </slot>
+          </div>x
+        </div>
+      </div>
+    </jjsnc-popup>
+  </transition>
+</template>
+
+<script>
+import jjsncPopup from "../popup/popup.vue";
+import jjsncInput from "../input/input.vue";
+import visibilityMixin from '../../common/mixins/visibility'
+import popupMixin from '../../common/mixins/popup'
+import localeMixin from '../../common/mixins/locale'
+
+const COMPONENT_NAME =  'jjsnc-dialog'
+const EVENT_CONFIRM = 'confirm'
+const EVENT_CANCEL = 'cancel'
+
+
+export default {
+  components: {
+    jjsncPopup,
+    jjsncInput
+  }
+};
+</script>
+
+<style lang="scss">
+.jjsnc-dialog-fade-enter-active {
+  animation: dialog-fadein 0.4s;
+}
+@keyframes dialog-fadein {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
+
+
