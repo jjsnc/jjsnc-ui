bb14869f385f9d52726a042f553cfd204c37e402
jjsnc <zhuowangyi@163.com>
2019-06-17 17:11:18 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,71 +1,18 @@
 <template>
   <div class="home">
-    <jjsnc-button  @click="showCascadePicker">Cascade Picker</jjsnc-button>
+    <jjsnc-button>Cascade Picker</jjsnc-button>
   </div>
 </template>
 
 <script>
-const cascadeData = [
-  {
-    value: 'Fruit',
-    text: 'Fruit',
-    children: [
-      {
-        value: 'Apple',
-        text: 'Apple',
-        children: [{ value: 1, text: 'One' }, { value: 2, text: 'Two' }]
-      },
-      {
-        value: 'Orange',
-        text: 'Orange',
-        children: [{ value: 3, text: 'Three'}, { value: 4, text: 'Four' }]
-      }
-    ]
-  },
-  {
-    value: 'Drink',
-    text: 'Drink',
-    children: [
-      {
-        value: 'Coffee',
-        text: 'Coffee',
-        children: [{ value: 1, text: 'One' }, { value: 2, text: 'Two' }]
-      },
-      {
-        value: 'Tea',
-        text: 'Tea',
-        children: [{ value: 1, text: 'One' }, { value: 3, text: 'Three'}]
-      }
-    ]
-  }
-]
 export default {
   name: "home",
   data() {
     return {};
   },
-  mounted () {
-    this.cascadePicker = this.$createCascadePicker({
-      title: 'Cascade Picker',
-      data: cascadeData,
-      selectedIndex: [0, 1, 0],
-      onSelect: this.selectHandle,
-      onCancel: this.cancelHandle
-    })
-  },
-  methods: {
-    showCascadePicker() {
-      this.cascadePicker.show()
-    },
-    selectHandle() {
-
-    },
-    cancelHandle() {
-    
-    }
-  },
-  components: {
-  }
+  mounted() {},
+  methods: {},
+  components: {}
 };
 </script>
 <style lang="scss">
@@ -74,7 +21,6 @@ export default {
   background: #efeff4;
   height: 100vh;
 }
-
 </style>
 
 
