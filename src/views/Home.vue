3ef8b5d8f87e4877dd618ca289605615a534a0e8
jjsnc <zhuowangyi@163.com>
2019-06-18 11:40:49 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <jjsnc-button @click="showAlert">Cascade Picker</jjsnc-button>
+    <jjsnc-button @click="showSlot">Cascade Picker</jjsnc-button>
   </div>
 </template>
 
@@ -12,22 +12,47 @@ export default {
   },
   mounted() {},
   methods: {
-    showAlert() {
-      this.dialog = this.$createDialog({
-        type: "prompt",
-        title: "我是标题",
-        prompt: {
-          value: "",
-          placeholder: "请输入"
+    showSlot() {
+      this.$createDialog(
+        {
+          type: "alert",
+          confirmBtn: {
+            text: "我知道了",
+            active: true
+          }
         },
-        onConfirm: (e, promptValue) => {
-          this.$createToast({
-            type: "warn",
-            time: 1000,
-            txt: `Prompt value: ${promptValue || ""}`
-          }).show();
+        createElement => {
+          return [
+            createElement(
+              "div",
+              {
+                class: {
+                  "my-title": true
+                },
+                slot: "title"
+              },
+              [
+                createElement("div", {
+                  class: {
+                    "my-title-img": true
+                  }
+                }),
+                createElement("p", "附近车少,优选出租车将来接您")
+              ]
+            ),
+            createElement(
+              "p",
+              {
+                class: {
+                  "my-content": true
+                },
+                slot: "content"
+              },
+              "价格仍按快车计算"
+            )
+          ];
         }
-      }).show();
+      ).show();
     }
   },
   components: {}
@@ -39,6 +64,15 @@ export default {
   background: #efeff4;
   height: 100vh;
 }
+.my-title-img {
+    height: 120px;
+    margin-bottom: 16px;
+    background: url('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560839219850&di=ff8af70c9c485f51fa443bdac9e4c370&imgtype=0&src=http%3A%2F%2Fcdn0.hbimg.cn%2Fstore%2Fwm%2Fpiccommon%2F1198%2F11984%2FD525A3C998BBFFB18A8DBC941A.jpg') no-repeat 50%;
+    background-size: 100%;
+}
+.my-content {
+  text-align: center;
+  }
 </style>
 
 
