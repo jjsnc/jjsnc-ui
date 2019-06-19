59bda5325daa2a080ed757ea7d3377e405df5ace
jjsnc <zhuowangyi@163.com>
2019-06-19 13:34:28 +0800

    init

---
+++ src/components/slide/slide.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="jjsnc-slide" ref="slide">
+    <div class="jjsnc-slide-group" ref="slideGroup">
+      <slot>
+        <jjsnc-slide-item
+          v-for="(item, index) in data"
+          :key="index"
+          @click.native="clickItem(item, index)"
+          :item="item"
+        ></jjsnc-slide-item>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import jjsncSlideItem from "./slide-item.vue";
+export default {
+  components: {
+    jjsncSlideItem
+  }
+};
+</script>
+<style lang="scss">
+</style>
+
+
