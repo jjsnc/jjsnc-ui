bc9b59d37b50c4cad319fd68ead1c170e5c87d6f
jjsnc <zhuowangyi@163.com>
2019-06-19 13:22:37 +0800

    init

---
+++ src/components/slide/slide-item.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="jjsnc-slide-item">
+    <slot>
+      <a href="item.url">
+        <img :src="item.image">
+      </a>
+    </slot>
+  </div>
+</template>
+<script>
+const COMPONENT_NAME = "jjsnc-slide-item";
+export default {
+  name: COMPONENT_NAME,
+  props: {
+    item: {
+      type: Object,
+      default() {
+        /* istanbul ignore next */
+        return {};
+      }
+    }
+  }
+};
+</script>
+<style lang="scss">
+.jjsnc-slide-item {
+  float: left;
+  box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  a > {
+    display: block;
+    height: 100%;
+    overflow: hidden;
+    text-decoration: none;
+    > img {
+      display: block;
+      height: 100%;
+    }
+  }
+}
+</style>
+
+
