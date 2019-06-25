a5657e80ceb85bd8894abb11096a5110b2e38965
jjsnc <zhuowangyi@163.com>
2019-06-25 15:39:37 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <jjsnc-button @click="showImagePreview">Show ImagePreview</jjsnc-button>
+    <div class="imgs-container">
+      <img :src="img" v-for="(img, index) in imgs" :key="img" @click="handleImgsClick(index)">
+    </div>
   </div>
 </template>
 
@@ -9,20 +11,32 @@ export default {
   name: "home",
   data() {
     return {
+      initialIndex: 0,
       imgs: [
         "https://wx1.sinaimg.cn/mw1024/686d7361ly1fpha0mpd5uj21hc0tyws2.jpg",
         "https://wx1.sinaimg.cn/mw1024/686d7361ly1fpha0ncnnej21hc0zetxo.jpg",
-        "https://wx1.sinaimg.cn/mw1024/686d7361ly1fpha0mqvu5j21hc0zkgzz.jpg",
-        "https://wx1.sinaimg.cn/mw1024/686d7361ly1fpha0m3ufuj21hc0zkqbj.jpg"
+        "https://wx1.sinaimg.cn/mw1024/686d7361ly1fpha0mqvu5j21hc0zkgzz.jpg"
       ]
     };
   },
   mounted() {},
   methods: {
-    showImagePreview() {
-      this.$createImagePreview({
-        imgs: this.imgs
-      }).show();
+    handleImgsClick(index) {
+      this.initialIndex = index;
+      const params = {
+        $props: {
+          imgs: this.imgs,
+          initialIndex: "initialIndex", // 响应式数据的key名
+          loop: false
+        },
+        $events: {
+          change: i => {
+            // 必须更新 initialIndex
+            this.initialIndex = i;
+          }
+        }
+      };
+      this.$createImagePreview({ ...params }).show();
     }
   },
   components: {}
@@ -46,6 +60,12 @@ export default {
 }
 .my-content {
   text-align: center;
+}
+.imgs-container {
+  img {
+    width: 150px;
+    height: 150px;
+  }
 }
 </style>
 
