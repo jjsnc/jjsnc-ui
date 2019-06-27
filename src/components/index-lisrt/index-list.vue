ef77882b8245c2814355e3b0fd0e501481b3e379
jjsnc <zhuowangyi@163.com>
2019-06-27 18:55:14 +0800

    init

---
+++ src/components/index-lisrt/index-list.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="jjsnc-index-list">
+    <jjsnc-scroll
+      ref="scroll"
+      :scroll-events="scrollEvents"
+      :options="scrollOptions"
+      :data="data"
+      @scroll="scroll"
+      @pulling-down="onPullingDown"
+      @pulling-up="onPullingUp"
+    >
+      <div class="jjsnc-index-list-content" ref="content">
+        <h1 class="jjsnc-index-list-title" v-if="title" ref="title" @click="titleClick">{{title}}</h1>
+        <ul>
+            <slot>
+                <jjsnc-index-list-group
+                v-for="(group, index) in data" :key="index" :group="group" @select="selectItem"
+                >
+                </jjsnc-index-list-group>
+                <template v-if="$slotss.pullup || $scopedSlots.pullup" slot="pullup">
+
+                </template>
+            </slot>
+        </ul>
+      </div>
+    </jjsnc-scroll>
+  </div>
+</template>
+
+<script>
+import JjsncScroll from "../scroll/scroll.vue";
+import JjsncIndexListGroup from  './index-list-group.vue'
+export default {
+  components: {
+    JjsncScroll,
+    JjsncIndexListGroup
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
+
+
