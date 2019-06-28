f5c027b7e795d846dfb55d0d0b0caa983b4b6cb8
jjsnc <zhuowangyi@163.com>
2019-06-28 10:12:44 +0800

    init

--- src/components/index-lisrt/index-list.vue
+++ src/components/index-lisrt/index-list.vue
@@ -12,24 +12,60 @@
       <div class="jjsnc-index-list-content" ref="content">
         <h1 class="jjsnc-index-list-title" v-if="title" ref="title" @click="titleClick">{{title}}</h1>
         <ul>
-            <slot>
-                <jjsnc-index-list-group
-                v-for="(group, index) in data" :key="index" :group="group" @select="selectItem"
-                >
-                </jjsnc-index-list-group>
-                <template v-if="$slotss.pullup || $scopedSlots.pullup" slot="pullup">
-
-                </template>
-            </slot>
+          <slot>
+            <jjsnc-index-list-group
+              v-for="(group, index) in data"
+              :key="index"
+              :group="group"
+              @select="selectItem"
+            ></jjsnc-index-list-group>
+          </slot>
         </ul>
       </div>
+      <template v-if="$slotss.pullup || $scopedSlots.pullup" slot="pullup">
+        <slot name="pullup" :pullUpLoad="props.pullUpLoad" :isPullUpLoad="props.isPullUpLoad"></slot>
+      </template>
+      <template v-if="$slots.pulldown  || $scopedSlots.pulldown" slot="pulldown" slot-scope="props">
+        <slot
+          name="pulldown"
+          :pullDownRefresh="props.pullDownRefresh"
+          :pullDownStyle="props.pullDownStyle"
+          :beforePullDown="props.beforePullDown"
+          :isPullingDown="props.isPullingDown"
+          :bubbleY="props.bubbleY"
+        ></slot>
+      </template>
     </jjsnc-scroll>
+    <div
+      v-if="navbar"
+      class="jjsnc-index-list-nav"
+      @touchstart="onShortcutTouchStart"
+      @touchmove.stop.prevent="onShortcutTouchMove"
+    >
+      <ul class="jjsnc-index-list-nav-list">
+        <li
+          v-for="(item, index) in shortcutList"
+          :key="index"
+          :data-index="index"
+          class="jjsnc-index-list-nav-item"
+          :class="{active:currentIndex===index}"
+        >
+          <slot name="nav-item" :item="item">{{item}}</slot>
+        </li>
+      </ul>
+    </div>
+    <div
+      ref="fixed"
+      v-show="fixedTitle"
+      v-html="fixedTitle"
+      class="jjsnc-index-list-fixed jjsnc-index-list-anchor"
+    ></div>
   </div>
 </template>
 
 <script>
 import JjsncScroll from "../scroll/scroll.vue";
-import JjsncIndexListGroup from  './index-list-group.vue'
+import JjsncIndexListGroup from "./index-list-group.vue";
 export default {
   components: {
     JjsncScroll,
