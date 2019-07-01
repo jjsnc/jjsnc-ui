014a245aaeb23a77d665d74cebb0498b13ee6348
jjsnc <zhuowangyi@163.com>
2019-07-01 15:00:38 +0800

    init

--- src/components/swipe/swipe-item.vue
+++ src/components/swipe/swipe-item.vue
@@ -129,9 +129,208 @@ export default {
         btn.style[transform] = `translate(${translate}px)`;
         btn.style[transitionDuration] = "0ms";
       }
+    },
+    _isInBtns(target) {
+      let parent = target;
+      let flag = false;
+      while (parent && parent.className.indexOf("jjsnc-swipe-item") < 0) {
+        if (parent.className.indexOf("jjsnc-swipe-btns") >= 0) {
+          flag = true;
+          break;
+        }
+        parent = parent.parentNode;
+      }
+      return flag;
+    },
+    _calculateBtnsWidth() {
+      let width = 0;
+      const len = this.cachedBtns.length;
+      for (let i = 0; i < len; i++) {
+        width += this.cachedBtns[i].width;
+      }
+      this.maxScrollX = -width;
+    },
+    _translate(x, useZ) {
+      let translateZ = useZ ? " translateZ(0)" : "";
+      this.scrollerStyle[transform] = `translate(${x}px, 0)${translateZ}`;
+      this.x = x;
+    },
+    _transitionProperty(property = "transform") {
+      this.scrollerStyle[transitionProperty] = property;
+    },
+    _transitionTimingFunction(easing) {
+      this.scrollerStyle[transitionTimingFunction] = easing;
+    },
+    _transitionTime(time = 0) {
+      this.scrollerStyle[transitionDuration] = `${time}ms`;
+    },
+    _getComputedPositionX() {
+      let matrix = window.getComputedStyle(this.$refs.swipeItem, null);
+      matrix = matrix[transform].split(")")[0].split(", ");
+      let x = +(matrix[12] || matrix[4]);
+      return x;
+    },
+    _translateBtns(time, easing, extend) {
+      /* istanbul ignore if */
+      if (this.btns.length === 0) {
+        return;
+      }
+
+      const len = this.$refs.btns.length;
+      let delta = 0;
+      let translate = 0;
+      for (let i = 0; i < len; i++) {
+        const btn = this.$$refs.btns[i];
+        if (this.state === STATE_GROW) {
+          translate = delta;
+        } else {
+          translate = 0;
+        }
+        delta += this.cachedBtns[i].width;
+        btn.style[transform] = `translate(${translate}px, 0) translateZ(0)`;
+        btn.style[transitionProperty] = "all";
+        btn.style[transitionTimingFunction] = easing;
+        btn.style[transitionDuration] = `${time}ms`;
+        if (extend) {
+          btn.style.width = `${this.cachedBtns[i].width}px`;
+        }
+      }
+    },
+    refresh() {
+      if (this.btns.length > 0) {
+        this._initCachedBtns();
+        this._calculateBtnsWidth();
+      }
+      this.endTime = 0;
+    },
+    shrink() {
+      this.stop();
+      this.state = STATE_SHRINK;
+      this.$nextTick(() => {
+        this.scrollTo(0, easingTime, easeOutQuart);
+        this._translateBtns(easingTime, easeOutQuart);
+      });
+    },
+    grow() {
+      this.state = STATE_GROW;
+      const extend = this.x < this.maxScrollX;
+      let easing = easeOutCubic;
+      this.scrollTo(this.maxScrollX, easingTime, easing);
+      this._translateBtns(easingTime, easing, extend);
+    },
+    scrollTo(x, time, easing) {
+      this._transitionProperty();
+      this._transitionTimingFunction(easing);
+      this._transitionTime(time);
+      this._translate(x, true);
+      if (time) {
+        this.isInTransition = true;
+      }
+    },
+    genBtnStyle(btn) {
+      return `background: ${btn.color}`;
+    },
+    clickItem() {
+      this.swipe.onItemClick(this.item, this.index);
+      this.$emit(EVENT_ITEM_CLICK, this.item, this.index);
+    },
+    clickBtn(btn) {
+      this.swipe.onBtnClick(btn, this.index);
+      this.$emit(EVENT_BTN_CLICK, btn, this.index);
+    },
+    stop() {
+      if (this.isInTransition) {
+        this.isInTransition = false;
+        let x = this.state === STATE_SHRINK ? 0 : this._getComputedPositionX();
+        this._translate(x);
+        this.$emit(EVENT_SCROLL, this.x);
+      }
+    },
+    onTouchStart(e) {
+      this.swipe.onItemActive(this.index);
+      this.$emit(EVENT_ACTIVE, this.index);
+      this.stop();
+      this.moved = false;
+      this.movingDirectionX = 0;
+      const point = e.touches[0];
+      this.pointX = point.pageX;
+      this.pointY = point.pageXY;
+      this.distX = 0;
+      this.distY = 0;
+      this.startX = this.x;
+      this._transitionTime();
+      this.startTime = getNow();
+      if (this.state === STATE_GROW && !this._isInBtns(e.target)) {
+        this.shrinkTimer = setTimeout(() => {
+          this.shrink();
+        }, 300);
+      }
+    },
+    onTouchMove(e) {
+      if (this.moved) {
+        clearTimeout(this.shrinkTimer);
+        e.stopPropagation();
+      }
+      /* istanbul ignore if */
+      if (this.isIntransition) {
+        return;
+      }
+      e.preventDefault();
+      const point = e.touches[0];
+      let deltaX = point.pageX - this.pointX;
+      let deltaY = (point.pageY = this.pointY);
+      this.pointX = point.pageX;
+      this.pointY = point.pageY;
+
+      this.distX += deltaX;
+      this.distY += deltaY;
+      let absDistX = Math.abs(this.distX);
+      let absDistY = Math.abs(this.distY);
+
+      if (absDistX + directionLockThreshold <= absDistY) {
+        return;
+      }
+      let timestamp = getNow();
+
+      if (
+        timestamp - this.endTime > momentumLimitTime &&
+        absDistX < momentumLimitDistance
+      ) {
+        return;
+      }
+      this.movingDirectionX =
+        deltaX > 0 ? DIRECTION_RIGHT : deltaX < 0 ? DIRECTION_LEFT : 0;
+
+      let newX = this.x + deltaX;
+      if (newX > 0) {
+        newX = 0;
+      }
+      if (newX < this.maxScrollX) {
+        newX = this.x + deltaX / 3;
+      }
+      if (!this.moved) {
+        this.moved = true;
+      }
+      this._translate(newX, true);
+
+      if (timestamp - this.startTime > momentumLimitTime) {
+        this.startTime = timestamp;
+        this.startX = this.x;
+      }
+
+      this.$emit(EVENT_SCROLL, this.x);
+    },
+    onTransitionEnd() {
+      this.isInTransition = false;
+      this._transitionTime();
+      this._translate(this.x);
     }
+  },
+  beforeDestroy() {
+    this.swipe.removeItem(this);
   }
 };
 </script>
+
 
 
