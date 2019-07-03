c04dfdb758a8a48df1f93c801504224b58f2634c
jjsnc <zhuowangyi@163.com>
2019-07-03 13:26:22 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,5 @@
 <template>
   <div class="home">
-    <jjsnc-scroll-nav-bar :current="current" :labels="labels" @change="changeHandler" />
-    <div class="side-container">
-      <jjsnc-scroll-nav-bar
-        direction="vertical"
-        :current="current"
-        :labels="labels"
-        :txts="txts"
-        @change="changeHandler"
-      >
-        <i slot-scope="props">{{props.txt}}</i>
-      </jjsnc-scroll-nav-bar>
-    </div>
   </div>
 </template>
 
@@ -20,38 +8,12 @@ export default {
   name: "home",
   data() {
     return {
-      current: "快车",
-      labels: [
-        "快车",
-        "小巴",
-        "专车",
-        "顺风车",
-        "代驾",
-        "公交",
-        "自驾租车",
-        "豪华车",
-        "二手车",
-        "出租车"
-      ],
-      txts: [
-        "1快车",
-        "2小巴",
-        "3专车",
-        "4顺风车",
-        "5代驾",
-        "6公交",
-        "7自驾租车",
-        "8豪华车",
-        "9二手车",
-        "10出租车"
-      ]
     };
   },
   computed: {},
   mounted() {},
   methods: {
-    changeHandler(cur) {
-      this.current = cur;
+    changeHandler() {
     }
   },
   components: {}
@@ -62,10 +24,7 @@ export default {
   height: 100vh;
   background: #edf0f4;
 }
-.side-container {
-  height: 300px;
-  margin-top: 20px;
-}
+
 </style>
 
 
