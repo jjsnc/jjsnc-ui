5a29c17944375d1e4716645f701c07a0c6139a99
jjsnc <zhuowangyi@163.com>
2019-07-04 14:28:36 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,5 @@
 <template>
   <div class="home">
-    <jjsnc-scroll-nav @change="changeHandler" :speed='100'>
-      <jjsnc-scroll-nav-panel v-for="item in data" :key="item.name" :label="item.name">
-        <ul>
-          <li v-for="(food, index) in item.foods" :key="index">
-            <div>
-              <img :src="food.icon" />
-              <p>{{food.name}}</p>
-            </div>
-          </li>
-        </ul>
-      </jjsnc-scroll-nav-panel>
-    </jjsnc-scroll-nav>
   </div>
 </template>
 
@@ -28,9 +16,6 @@ export default {
   computed: {},
   mounted() {},
   methods: {
-    changeHandler(label) {
-      // console.log("changed to:", label);
-    }
   },
   components: {}
 };
@@ -39,34 +24,6 @@ export default {
 .home {
   height: 100vh;
   background: #edf0f4;
-}
-.jjsnc-scroll-nav-panel-title{
-  padding: 15px;
-  font-size: 16px;
-  text-align: center;
-  background: #fff;
-}
-
- .jjsnc-scroll-nav-panel ul {
-    overflow: hidden;
-    font-size: 14px;
-    line-height: 1.4;
-    color: #666;
-    background: #fff;
-}
-.jjsnc-scroll-nav-panel li {
-    float: left;
-    width: 50%;
-}
-
-.jjsnc-scroll-nav-panel li div {
-    width: 114px;
-    margin: 0 auto 15px;
-}
-
-.jjsnc-scroll-nav-panel img {
-    width: 114px;
-    height: 114px;
 }
 
 </style>
