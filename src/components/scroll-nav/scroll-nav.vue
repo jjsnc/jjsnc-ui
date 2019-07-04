7c1264d24c1c3ae9b9aa9a1434bc1941c0ecc59f
jjsnc <zhuowangyi@163.com>
2019-07-04 13:36:08 +0800

    init

---
+++ src/components/scroll-nav/scroll-nav.vue
@@ -0,0 +1,264 @@
+<template>
+  <div class="jjsnc-scroll-nav" :class="{'jjsnc-scroll-nav_side':side}">
+    <jjsnc-sticky ref="sticky" :pos="scrollY" @change="stickyChangeHandler">
+      <jjsnc-scroll
+        ref="scroll"
+        :scroll-events="scrollEvents"
+        :data="data"
+        @scroll="scrollHandler"
+        @scroll-end="scrollEndHandler"
+      >
+        <slot name="prepend"></slot>
+        <div class="jjsnc-scroll-nav-main">
+          <jjsnc-sticky-ele ref="navBarEle" ele-key="jjsnc-scroll-nav-bar">
+            <slot name="bar" :txts="barTxts" :labels="labels" :current="active">
+              <jjsnc-scroll-nav-bar
+                :direction="barDirection"
+                :txts="barTxts"
+                :labels="labels"
+                :current="active"
+              ></jjsnc-scroll-nav-bar>
+            </slot>
+          </jjsnc-sticky-ele>
+          <jjsnc-sticky
+            ref="pageSticky"
+            :offset="pageStickyOffset"
+            :pos="scrollY"
+            @change="pageStickyChangeHandler"
+          >
+            <div class="jjsnc-scroll-nav-panels">
+              <slot></slot>
+            </div>
+            <template slot="fixed" v-if="!side">
+              <slot></slot>
+            </template>
+          </jjsnc-sticky>
+        </div>
+      </jjsnc-scroll>
+    </jjsnc-sticky>
+  </div>
+</template>
+
+<script>
+import scrollMixin from "../../common/mixins/scroll";
+
+import jjsncSticky from "../sticky/sticky.vue";
+import jjsncStickyEle from "../sticky/sticky-ele.vue";
+import jjsncScroll from "../scroll/scroll.vue";
+import jjsncScrollNavBar from "../scroll-nav-bar/scroll-nav-bar.vue";
+
+const DIRECTION_H = "horizontal";
+const DIRECTION_V = "vertical";
+const COMPONENT_NAME = "jjsnc-scroll-nav";
+const EVENT_CHANGE = "change";
+const EVENT_STICKY_CHANGE = "sticky-change";
+
+export default {
+  name: COMPONENT_NAME,
+  provide() {
+    return {
+      scrollNav: this
+    };
+  },
+  mixins: [scrollMixin],
+  props: {
+    data: {
+      type: Number,
+      default: 300
+    },
+    side: {
+      type: Boolean,
+      default: false
+    },
+    current: {
+      type: [String, Number],
+      default: " "
+    }
+  },
+  data() {
+    return {
+      scrollEvents: ["scroll", "scroll-end"],
+      scrollY: 0,
+      panels: [],
+      active: this.current,
+      pageStickyOffset: 0
+    };
+  },
+  computed: {
+    labels() {
+      return this.panels.map(panel => panel.label);
+    },
+    barTxts() {
+      return this.panels.map(panel => panel.title);
+    },
+    barDirection() {
+      return this.side ? DIRECTION_V : DIRECTION_H;
+    }
+  },
+  watch: {
+    current(newVal) {
+      this.stickyCurrent = newVal;
+      this.active = newVal;
+      this.jumpTo(newVal);
+    },
+    active(newVal) {
+      this.$emit(EVENT_CHANGE, newVal);
+    }
+  },
+  created() {
+    this.navBar = null;
+    this.stickyCurrent = this.current;
+  },
+  mounted() {
+    this.$nextTick(() => {
+      if (this.side) {
+        const fixedEle = this.$refs.pageSticky.$refs.fixedEle;
+        this.$refs.scroll.$el.appendChild(fixedEle);
+      }
+      if (!this.current) {
+        this.active = this.stickyCurrent = this.labels[0];
+      }
+      this.refresh();
+      this.jumpTo(this.current);
+    });
+  },
+  methods: {
+    refresh() {
+      this.navBar && this.navBar.refresh();
+      this.$refs.sticky.refresh();
+      this.$refs.pageSticky.refresh();
+      this.pageStickyOffset = this.side
+        ? 0
+        : this.$refs.navBarEle.$el.offsetHeight;
+      this.$refs.scroll.refresh();
+    },
+    setBart(bar) {
+      this.navBar = bar;
+    },
+    barChange(label) {
+      this.active = label;
+      // waiting touchend
+      // so the inner scroll won't affect the touchend logic
+      setTimeout(() => {
+        this.jumpTo(label);
+      });
+    },
+    jumpTo(label) {
+      if (!label) {
+        return;
+      }
+      const panel = this.getPanel(label);
+      if (panel) {
+        this._jumping = true;
+        const offset = this.pageStickyOffset;
+        this.$$refs.scroll.scrollToElement(
+          panel.$el,
+          this.speed,
+          0,
+          this.side ? offset : -offset
+        );
+      }
+    },
+    getPanel(label) {
+      let panel = null;
+      this.panels.some(_panel => {
+        if (_panel.label === label) {
+          panel = _panel;
+          return true;
+        }
+      });
+      return panel;
+    },
+    pageStickyChangeHandler(current) {
+      if (current === "") {
+        current = this.labels[0];
+      }
+      this.stickyCurrent = current;
+      if (this._panel) {
+        return;
+      }
+      this.active = current;
+    },
+    stickyChangeHandler(current) {
+      // when sticky change waiting dom changed
+      // then refresh navBar(navBar will get correct height)
+      this.$nextTick(() => {
+        this.navBar && this.navBar.refresh();
+      });
+      this.$emit(EVENT_STICKY_CHANGE, current);
+    },
+    scrollHandler(pos) {
+      this.scrollY = -pos;
+      if (!this._jumping) {
+        this.active = this.stickyCurrent;
+      }
+    },
+    scrollEndHandler() {
+      this._jumping = false;
+    },
+    addPanel(panel) {
+      this.panels.push(panel);
+    },
+    removePanel(panel) {
+      const i = this.panels.indexOf(panel);
+      this.panels.splice(i, 1);
+    }
+  },
+  components: {
+    jjsncSticky,
+    jjsncStickyEle,
+    jjsncScroll,
+    jjsncScrollNavBar
+  }
+};
+</script>
+<style lang="scss">
+.jjsnc-scroll-nav {
+  position: relative;
+  height: 100%;
+  overflow: hidden;
+  .jjsnc-sticky-content {
+    height: 100%;
+  }
+}
+
+.jjsnc-scroll-nav-main {
+  overflow: hidden;
+  > .jjsnc-sticky {
+    position: static;
+  }
+}
+
+.jjsnc-scroll-nav_side {
+  > .jjsnc-sticky {
+    display: flex;
+    > .jjsnc-scroll-wrapper {
+      flex: 1;
+    }
+    > .jjsnc-sticky-fixed {
+      position: relative;
+      height: 100%;
+      order: -1;
+    }
+  }
+  .jjsnc-scroll-nav-main {
+    > .jjsnc-sticky-ele {
+      float: left;
+    }
+    > .jjsnc-sticky {
+      overflow: hidden;
+    }
+  }
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
