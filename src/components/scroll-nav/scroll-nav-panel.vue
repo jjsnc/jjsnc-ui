190d6c5d529f433ffd3e644b12b0ea7f05ca9364
jjsnc <zhuowangyi@163.com>
2019-07-04 10:35:09 +0800

    init

---
+++ src/components/scroll-nav/scroll-nav-panel.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="jjsnc-scroll-nav-panel">
+    <jjsnc-sticky-ele :ele-key="label">
+      <h2 class="jjsnc-scroll-nav-panel-title" v-html="title"></h2>
+    </jjsnc-sticky-ele>
+    <slot></slot>
+  </div>
+</template>
+<script>
+import jjsncStickyEle from "../sticky/sticky-ele.vue";
+
+const COMPONENT_NAME = "jjsnc-scroll-nav-panel";
+export default {
+  name: COMPONENT_NAME,
+  inject: ["scrollNav"],
+  props: {
+    label: {
+      type: [String, Number],
+      required: true
+    },
+    title: {
+      type: [String, Number],
+      default() {
+        /* istanbul ignore next */
+        return this.label;
+      }
+    }
+  },
+  mounted() {
+    this.scrillNav.addPane(this);
+  },
+  beforeDestroy() {
+    this.scrollNav.removePanel(this);
+  },
+  components: {
+    jjsncStickyEle
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
+
