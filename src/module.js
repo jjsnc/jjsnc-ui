5a3dabb2b0753c56bf0ac12745af9f9d53dd830e
jjsnc <zhuowangyi@163.com>
2019-07-10 15:49:31 +0800

    init

--- src/module.js
+++ src/module.js
@@ -1,126 +1,141 @@
 
-// locale
-import Locale from './modules/locale'
 
-// Basic
-import Button from './modules/button'
-import Loading from './modules/loading'
-import Tip from './modules/tip'
-import Toolbar from './modules/toolbar'
-import TabBar from './modules/tab-bar'
-import TabPanels from './modules/tab-panels'
+import { processComponentName } from './common/helpers/util'
+import {
+    /* eslint-disable no-unused-vars */
+    // Style,
+    // locale
+    Locale,
+    // basic
+    Button,
+    Loading,
+    Tip,
+    Toolbar,
+    TabBar,
+    TabPanels,
+    // form
+    Checkbox,
+    CheckboxGroup,
+    Checker,
+    Radio,
+    RadioGroup,
+    Input,
+    Textarea,
+    Select,
+    Switch,
+    Rate,
+    Validator,
+    Upload,
+    Form,
+    // popup
+    Popup,
+    Toast,
+    Picker,
+    CascadePicker,
+    // DatePicker,
+    // TimePicker,
+    // SegmentPicker,
+    Dialog,
+    ActionSheet,
+    // Drawer,
+    ImagePreview,
+    // scroll
+    Scroll,
+    Slide,
+    IndexList,
+    Swipe,
+    Sticky,
+    ScrollNav,
+    ScrollNavBar,
+    RecycleList,
+    // module
+    BetterScroll,
+    createAPI
+} from './module'
 
-// Form
-import Checkbox from './modules/checkbox'
-import CheckboxGroup from './modules/checkbox-group'
-import Checker from './modules/checker'
-import RadioGroup from './modules/radio-group'
-import Input from './modules/input'
-import Textarea from './modules/textarea'
-import Select from './modules/select'
-import Switch from './modules/switch'
-import Rate from './modules/rate'
-import Validator from './modules/validator'
-import Upload from './modules/upload'
-import Form from './modules/form'
 
-// Popup
-import Popup from './modules/popup'
-import Toast from './modules/toast'
-import Picker from './modules/picker'
-import CascadePicker from './modules/cascade-picker'
-// import DatePicker from './modules/date-picker'
-// import TimePicker from './modules/time-picker'
-// import SegmentPicker from './modules/segment-picker'
-import Dialog from './modules/dialog'
-import ActionSheet from './modules/action-sheet'
-// import Drawer from './modules/drawer'
-import ImagePreview from './modules/image-preview'
 
-// Scroll
-import Scroll from './modules/scroll'
-import Slide from './modules/slide'
-import IndexList from './modules/index-list'
-import Swipe from './modules/swipe'
-import Sticky from './modules/sticky'
-import ScrollNav from './modules/scroll-nav'
-import ScrollNavBar from './modules/scroll-nav-bar'
-import RecycleList from './modules/recycle-list'
+const components = [
+    // locale
+    Locale,
+    // basic
+    Button,
+    Loading,
+    Tip,
+    Toolbar,
+    TabBar,
+    TabPanels,
+    // form
+    Checkbox,
+    CheckboxGroup,
+    Checker,
+    Radio,
+    RadioGroup,
+    Input,
+    Textarea,
+    Select,
+    Switch,
+    Rate,
+    Validator,
+    Upload,
+    Form,
+    // popup
+    Popup,
+    Toast,
+    Picker,
+    CascadePicker,
+    // DatePicker,
+    // TimePicker,
+    // SegmentPicker,
+    Dialog,
+    ActionSheet,
+    // Drawer,
+    ImagePreview,
+    // scroll
+    Scroll,
+    Slide,
+    IndexList,
+    Swipe,
+    Sticky,
+    ScrollNav,
+    ScrollNavBar,
+    RecycleList
+]
 
-// module
-import BetterScroll from './modules/better-scroll'
-import createAPI from './modules/create-api'
 
-const FormGroup = Form.Group
-const FormItem = Form.Item
-const SlideItem = Slide.Item
-const Radio = RadioGroup.Radio
-const SwipeItem = Swipe.Item
-// const DrawerPanel = Drawer.Panel
-// const DrawerItem = Drawer.Item
-const StickyEle = Sticky.Ele
-const ScrollNavPanel = ScrollNav.Panel
-const Tab = TabBar.Tab
-const TabPanel = TabPanels.Panel
-const CheckerItem = Checker.Item
-
-export {
-  // locale
-  Locale,
-  // basic
-  Button,
-  Loading,
-  Tip,
-  Toolbar,
-  TabBar,
-  Tab,
-  TabPanels,
-  TabPanel,
-  // form
-  Checkbox,
-  CheckboxGroup,
-  CheckerItem,
-  Checker,
-  Radio,
-  RadioGroup,
-  Input,
-  Textarea,
-  Select,
-  Switch,
-  Rate,
-  Validator,
-  Upload,
-  FormItem,
-  FormGroup,
-  Form,
-  // popup
-  Popup,
-  Toast,
-  Picker,
-  CascadePicker,
-  // DatePicker,
-  // TimePicker,
-  // SegmentPicker,
-  Dialog,
-  ActionSheet,
-  // DrawerItem,
-  // DrawerPanel,
-  // Drawer,
-  ImagePreview,
-  // scroll
-  Scroll,
-  SlideItem,
-  Slide,
-  IndexList,
-  SwipeItem,
-  Swipe,
-  StickyEle,
-  Sticky,
-  ScrollNavPanel,
-  ScrollNav,
-  ScrollNavBar,
-  RecycleList,
-  // module
-  BetterScroll,
-  createAPI
+function install(Vue) {
+    if (install.installed) {
+        return
+    }
+    install.installed = true
+    components.forEach((Component) => {
+        // ignore radio
+        if (Component === Radio) {
+            return
+        }
+        Component.install(Vue)
+    })
 }
+
+const Jjsnc = {
+    /* eslint-disable no-undef */
+    version: '1.12.26',
+    install,
+    BScroll: BetterScroll,
+    createAPI
+}
+
+components.forEach((Component) => {
+    const name = processComponentName(Component, {
+        firstUpperCase: true
+    })
+    Jjsnc[name] = Component
+})
+
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(install)
+}
+
+export default Jjsnc
+
+
